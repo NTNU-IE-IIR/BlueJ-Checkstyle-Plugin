2eb7df6f0af62fd07e1b2acf1f8a8c3d1af28925
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2021-11-08 14:31:04 +0100

    feat: implement Checkstyle Service and Listener
    Implemented a CheckerService and a CheckerListener. Service should be
    used by other classes when a Check should be run. The Listener is
    responsible for feeding the ViolationManager with Violations.

---
+++ src/main/java/no/ntnu/iir/bluej/checkstyle/checker/CheckerListener.java
@@ -0,0 +1,76 @@
+package no.ntnu.iir.bluej.checkstyle.checker;
+
+import bluej.extensions2.editor.TextLocation;
+import com.puppycrawl.tools.checkstyle.api.AuditEvent;
+import com.puppycrawl.tools.checkstyle.api.AuditListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import no.ntnu.iir.bluej.checkstyle.core.violations.Violation;
+import no.ntnu.iir.bluej.checkstyle.core.violations.ViolationManager;
+
+/**
+ * Represents a CheckerListener.
+ * Responsible for adding errors to the ViolationManager.
+ */
+public class CheckerListener implements AuditListener {
+  private ViolationManager violationManager;
+
+  public CheckerListener(ViolationManager violationManager) {
+    this.violationManager = violationManager;
+  }
+
+  /**
+   * Fired when Checkstyle finds a violation in the file being processed.
+   * Adds violations to the ViolationManager instance. 
+   */
+  @Override
+  public void addError(AuditEvent auditEvent) {
+    String fileName = auditEvent.getFileName();
+    File file = new File(fileName);
+    Violation violation = new Violation(
+        auditEvent.getMessage(), 
+        file, 
+        new TextLocation(auditEvent.getLine(), auditEvent.getColumn())
+    );
+
+    List<Violation> violations = violationManager.getViolations(file.getName());
+    if (violations != null) {
+      violations.add(violation);
+    } else {
+      ArrayList<Violation> violationList = new ArrayList<>();
+      violationList.add(violation);
+      violationManager.addViolations(fileName, violationList);
+    }
+  }
+
+  @Override
+  public void addException(AuditEvent auditEvent, Throwable throwable) {
+    // do nothing
+  }
+
+  @Override
+  public void auditFinished(AuditEvent auditEvent) {
+    // do nothing
+  }
+
+  @Override
+  public void auditStarted(AuditEvent auditEvent) {
+    // do nothing
+  }
+
+  @Override
+  public void fileFinished(AuditEvent auditEvent) {
+    // do nothing
+  }
+
+  /**
+   * Fired when a file is starting to get processed.
+   * Should delete the old entry, in order to get a new List which 
+   * reflects the current state of the file.
+   */
+  @Override
+  public void fileStarted(AuditEvent auditEvent) {
+    this.violationManager.removeViolations(auditEvent.getFileName());
+  }
+}
---
+++ src/main/java/no/ntnu/iir/bluej/checkstyle/checker/CheckerService.java
@@ -0,0 +1,79 @@
+package no.ntnu.iir.bluej.checkstyle.checker;
+
+import com.puppycrawl.tools.checkstyle.Checker;
+import com.puppycrawl.tools.checkstyle.ConfigurationLoader;
+import com.puppycrawl.tools.checkstyle.PropertiesExpander;
+import com.puppycrawl.tools.checkstyle.api.CheckstyleException;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Properties;
+import no.ntnu.iir.bluej.checkstyle.core.violations.ViolationManager;
+
+/**
+ * Represents a Checker service.
+ * Responsible for handling requests to check files using a Checkstyle checker.
+ */
+public class CheckerService {
+  private ViolationManager violationManager;
+  private Checker checker;
+
+  public CheckerService(ViolationManager violationManager) {
+    this.violationManager = violationManager;
+  }
+  
+  /**
+   * Initializes the CheckerService.
+   */
+  public void init() {
+    this.checker = new Checker();
+    
+    this.checker.setBasedir(null);
+    this.checker.setModuleClassLoader(Checker.class.getClassLoader());
+    try {
+      this.setConfiguration();
+    } catch (Exception e) {
+      // TODO: Show error message/dialog to the user to let them know something is wrong
+      // Likely caused by a faulty config file used
+      e.printStackTrace();
+    }
+
+    this.checker.addListener(
+        new CheckerListener(this.violationManager)
+    );
+  }
+
+  /**
+   * Configures Checkstyle to use given configuration.
+   * TODO: Read Configuration from BlueJ preferences.
+   * 
+   * @throws IOException if an error reading the configuration file occurs.
+   * @throws CheckstyleException if an error condition within Checkstyle occurs.
+   */
+  public void setConfiguration() throws IOException, CheckstyleException {
+    Properties checkstyleProperties = new Properties();
+    this.checker.configure(ConfigurationLoader.loadConfiguration(
+        this.getClass().getClassLoader().getResource("config/google_checks.xml").toString(),
+        new PropertiesExpander(checkstyleProperties)
+    ));
+  }
+
+
+  /**
+   * Checks a list of files.
+   * 
+   * @param filesToCheck a List of Files to check.
+   * @param charset the files charset encoding.
+   * 
+   * @throws UnsupportedEncodingException if an unsupported encoding is used.
+   * @throws CheckstyleException if an error condition within Checkstyle occurs.
+   */
+  public void checkCode(
+      List<File> filesToCheck, 
+      String charset
+  ) throws UnsupportedEncodingException, CheckstyleException {
+    this.checker.setCharset(charset);
+    this.checker.process(filesToCheck);
+  }
+}
