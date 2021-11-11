d35f0d7e48a6f83e4e96119f5d37dfbeec10e49d
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2021-11-11 19:15:36 +0100

    feat: add FilesChangeListener
    Added a FilesChangeListener that should handle events when a Class file
    is changed within BlueJ.

---
+++ src/main/java/no/ntnu/iir/bluej/checkstyle/core/files/FilesChangeListener.java
@@ -0,0 +1,64 @@
+package no.ntnu.iir.bluej.checkstyle.core.files;
+
+import bluej.extensions2.BClass;
+import bluej.extensions2.event.ClassEvent;
+import bluej.extensions2.event.ClassListener;
+import no.ntnu.iir.bluej.checkstyle.core.checker.ICheckerService;
+import no.ntnu.iir.bluej.checkstyle.core.violations.ViolationManager;
+
+/**
+ * Represents a FilesChangeListener, listening for file changes in class files.
+ * Implements BlueJ's ClassListener interface, and handles the events triggered by BlueJ.
+ * Should remove old Violations from the ViolationsManager, and if necessary reprocess the file.
+ */
+public class FilesChangeListener implements ClassListener {
+  private ViolationManager violationManager;
+  private ICheckerService checkerService;
+
+  public FilesChangeListener(ViolationManager violationManager, ICheckerService checkerService) {
+    this.violationManager = violationManager;
+    this.checkerService = checkerService;
+  }
+
+  /**
+   * Helper method for processing a single class file.
+   * 
+   * @param fileName the name of the file to check.
+   * @param blueClass the BClass instance where a change was detected in.
+   */
+  private void processFile(String fileName, BClass blueClass) {
+    this.violationManager.removeViolations(fileName);
+    try {
+      this.checkerService.checkFile(blueClass.getJavaFile(), "utf-8");
+    } catch (Exception e) {
+      // TODO: Show error message/dialog to the user to let them know something is wrong.
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Removes violations for the old file name and reprocess with new name.
+   */
+  @Override
+  public void classNameChanged(ClassEvent classEvent) {
+    this.processFile(classEvent.getOldName(), classEvent.getBClass());    
+  }
+
+  /**
+   * Removes violations for the removed class file.
+   */
+  @Override
+  public void classRemoved(ClassEvent classEvent) {
+    this.violationManager.removeViolations(classEvent.getOldName());
+  }
+
+  /**
+   * Reprocesses the changed class file.
+   */
+  @Override
+  public void classStateChanged(ClassEvent classEvent) {
+    BClass blueClass = classEvent.getBClass();
+    this.processFile(blueClass.getName(), blueClass);
+  }
+  
+}
