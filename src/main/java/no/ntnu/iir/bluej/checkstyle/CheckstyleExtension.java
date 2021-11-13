191d385844e418f41ba21c7bfeaa37e991d5c340
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2021-11-13 02:04:36 +0100

    feat: move to PackageEventHandler implementation
    Changed to the PackageEventHandler implementation.

--- src/main/java/no/ntnu/iir/bluej/checkstyle/CheckstyleExtension.java
+++ src/main/java/no/ntnu/iir/bluej/checkstyle/CheckstyleExtension.java
@@ -3,8 +3,7 @@ package no.ntnu.iir.bluej.checkstyle;
 import bluej.extensions2.BlueJ;
 import bluej.extensions2.Extension;
 import java.util.logging.Logger;
-import no.ntnu.iir.bluej.checkstyle.core.handlers.AppEventHandler;
-import no.ntnu.iir.bluej.checkstyle.core.ui.AuditWindow;
+import no.ntnu.iir.bluej.checkstyle.core.handlers.PackageEventHandler;
 import no.ntnu.iir.bluej.checkstyle.core.violations.ViolationManager;
 
 public class CheckstyleExtension extends Extension {
@@ -14,11 +13,8 @@ public class CheckstyleExtension extends Extension {
   public void startup(BlueJ blueJ) {
     LOGGER.info("Starting checkstyle4bluej");
 
-    AuditWindow auditWindow = new AuditWindow("checkstyle4bluej");
     ViolationManager violationManager = new ViolationManager();
-    violationManager.addListener(auditWindow);
-
-    blueJ.addApplicationListener(new AppEventHandler(auditWindow));
+    blueJ.addPackageListener(new PackageEventHandler("checkstyle4bluej", violationManager));
   }
 
   @Override
