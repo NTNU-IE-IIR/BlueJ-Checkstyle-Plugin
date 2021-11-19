4c48928474ff1956b32b0eafdd7679ca4e8f9913
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2021-11-19 11:06:41 +0100

    refactor!: change to work independently with static methods
    In stead of having an instance for each package opened, we can use these
    methods staticly. The behaviour should not depend on which package, it should be acting upon a specified BClass parameter instead. The BClass is correctly placed in its respective package.

--- src/main/java/no/ntnu/iir/bluej/checkstyle/core/editor/EditorNotifier.java
+++ src/main/java/no/ntnu/iir/bluej/checkstyle/core/editor/EditorNotifier.java
@@ -1,12 +1,10 @@
 package no.ntnu.iir.bluej.checkstyle.core.editor;
 
 import bluej.extensions2.BClass;
-import bluej.extensions2.BPackage;
 import bluej.extensions2.PackageNotFoundException;
 import bluej.extensions2.ProjectNotOpenException;
 import bluej.extensions2.editor.JavaEditor;
 import bluej.extensions2.editor.TextLocation;
-import java.io.File;
 
 /**
  * Represents a utility class for notifying the BlueJ editor.
@@ -14,53 +12,28 @@ import java.io.File;
  * errors/red lines under a TextLocation and highlighting text on request.
  */
 public class EditorNotifier {
-  private BPackage bluePackage;
-
-  public EditorNotifier(BPackage bluePackage) {
-    this.bluePackage = bluePackage;
-  }
+  private EditorNotifier() {}
 
   /**
-   * Highlights a TextLocation in a File in the BlueJ editor.
-   * Attempts to find the BClass in the BPackage, opens the editor 
-   * and sets the selection to the TextLocation.
+   * Highlights a line in a file in the BlueJ editor.
+   * Fetches the JavaEditor proxy or opens a new one.
    * 
-   * @param file the File to highlight text in
+   * @param blueClass the BClass to fetch JavaEditor from
    * @param textLocation the TextLocation of where to highlight
    * 
    * @throws ProjectNotOpenException project was not open
    * @throws PackageNotFoundException package could not be found
-   * @throws ClassNotFoundException if the Class file could not be found
    */
-  public void highlightLine(
-      File file, 
+  public static void highlightLine(
+      BClass blueClass, 
       TextLocation textLocation
-  ) throws ProjectNotOpenException, PackageNotFoundException, ClassNotFoundException {
-
-    BClass[] blueClasses = this.bluePackage.getClasses();
-    BClass foundClass = null;
-    String targetAbsolutePath = file.getAbsolutePath();
-    
-    // search for the BClass matching the input File
-    for (int i = 0; (i < blueClasses.length && foundClass == null); i++) {
-      BClass currentClass = blueClasses[i];
-      if (targetAbsolutePath.equals(currentClass.getJavaFile().getAbsolutePath())) {
-        foundClass = currentClass;
-      }
-    }
-
-    if (foundClass != null) {
-      JavaEditor fileEditor = foundClass.getJavaEditor();
-      int lineLength = fileEditor.getLineLength(textLocation.getLine());
-      fileEditor.setSelection(
-          textLocation,
-          new TextLocation(textLocation.getLine(), lineLength)
-      );
-      fileEditor.setVisible(true);
-    } else {
-      // TODO: not sure if this is the right exception...
-      throw new ClassNotFoundException("Could not find a class source from File");
-    }
-    
+  ) throws ProjectNotOpenException, PackageNotFoundException {
+    JavaEditor fileEditor = blueClass.getJavaEditor();
+    int lineLength = fileEditor.getLineLength(textLocation.getLine());
+    fileEditor.setVisible(true);
+    fileEditor.setSelection(
+        textLocation,
+        new TextLocation(textLocation.getLine(), lineLength)
+    );
   }
 }
