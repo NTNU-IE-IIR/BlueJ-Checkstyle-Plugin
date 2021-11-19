62a1bd719356654caf8d5485f177bcca94341771
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2021-11-19 11:55:49 +0100

    feat: implement method for showing Diagnostics from a Violation
    showDiagnostic method that utilizes the EditorBridge in BlueJ in order
    to get the Editor instance from the JavaEditor proxy object. Creates a
    Diagnostic instance based off the Violation provided to the function.

--- src/main/java/no/ntnu/iir/bluej/checkstyle/core/editor/EditorNotifier.java
+++ src/main/java/no/ntnu/iir/bluej/checkstyle/core/editor/EditorNotifier.java
@@ -1,10 +1,16 @@
 package no.ntnu.iir.bluej.checkstyle.core.editor;
 
+import bluej.compiler.CompileType;
+import bluej.compiler.Diagnostic;
+import bluej.compiler.Diagnostic.DiagnosticOrigin;
+import bluej.editor.Editor;
 import bluej.extensions2.BClass;
 import bluej.extensions2.PackageNotFoundException;
 import bluej.extensions2.ProjectNotOpenException;
+import bluej.extensions2.editor.EditorBridge;
 import bluej.extensions2.editor.JavaEditor;
 import bluej.extensions2.editor.TextLocation;
+import no.ntnu.iir.bluej.checkstyle.core.violations.Violation;
 
 /**
  * Represents a utility class for notifying the BlueJ editor.
@@ -17,12 +23,13 @@ public class EditorNotifier {
   /**
    * Highlights a line in a file in the BlueJ editor.
    * Fetches the JavaEditor proxy or opens a new one.
+   * Sets the JavaEditor to be visible, and then sets the selection.
    * 
-   * @param blueClass the BClass to fetch JavaEditor from
+   * @param blueClass the BClass to fetch the JavaEditor proxy object from
    * @param textLocation the TextLocation of where to highlight
    * 
-   * @throws ProjectNotOpenException project was not open
-   * @throws PackageNotFoundException package could not be found
+   * @throws ProjectNotOpenException if the BCLass' project was not open
+   * @throws PackageNotFoundException if the BClass' package was not found
    */
   public static void highlightLine(
       BClass blueClass, 
@@ -34,6 +41,45 @@ public class EditorNotifier {
     fileEditor.setSelection(
         textLocation,
         new TextLocation(textLocation.getLine(), lineLength)
+    );
+  }
+
+  /**
+   * Shows a diagnostic message underneath a specified line.
+   * Uses the EditorBridge to get Editor from JavaEditor proxy object.
+   * Utilizes the Editor object to show a Diagnostic message to the user.
+   * 
+   * @param blueClass the BClass to fetch the JavaEditor proxy object from
+   * @param violation the Violation to show diagnostic message about
+   */
+  public static void showDiagnostic(
+      BClass blueClass, 
+      Violation violation
+  ) throws ProjectNotOpenException, PackageNotFoundException {
+    JavaEditor javaEditor = blueClass.getJavaEditor();
+    javaEditor.setVisible(true);
+    Editor editor = EditorBridge.getJavaEditor(javaEditor);
+    TextLocation textLocation = violation.getLocation();
+    int lineLength = javaEditor.getLineLength(textLocation.getLine());
+
+    // TODO: Check effect of identifier
+    Diagnostic diagnosticMessage = new Diagnostic(
+        1,                                      // type
+        violation.getSummary(),                 // message
+        violation.getFile().getName(),          // fileName
+        Long.valueOf(textLocation.getLine()),   // startLine
+        Long.valueOf(textLocation.getColumn()), // startColumn
+        Long.valueOf(textLocation.getLine()),   // endLine
+        Long.valueOf(lineLength),               // endColumn
+        DiagnosticOrigin.UNKNOWN,               // origin
+        1                                       // identifier
+    );
+
+    // TODO: Check effect of errorIndex
+    editor.displayDiagnostic(
+        diagnosticMessage,    // diagnostic
+        0,                    // errorIndex
+        CompileType.EXTENSION // compileType
     );
   }
 }
