86f8d85111392ee23a0970cfeb24cd32be3e2ab6
thedatasnok <47036430+thedatasnok@users.noreply.github.com>
2022-02-04 07:14:53 +0100

    feat: add checking for correct ExtensionsAPI to decide if compatible

--- src/main/java/no/ntnu/iir/bluej/checkstyle/CheckstyleExtension.java
+++ src/main/java/no/ntnu/iir/bluej/checkstyle/CheckstyleExtension.java
@@ -2,6 +2,8 @@ package no.ntnu.iir.bluej.checkstyle;
 
 import bluej.extensions2.BlueJ;
 import bluej.extensions2.Extension;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.logging.Logger;
 import no.ntnu.iir.bluej.checkstyle.checker.CheckerListener;
 import no.ntnu.iir.bluej.checkstyle.checker.CheckerService;
@@ -55,12 +57,23 @@ public class CheckstyleExtension extends Extension {
 
   @Override
   public boolean isCompatible() {
-    return true;
+    int versionMajor = Extension.getExtensionsAPIVersionMajor();
+    int versionMinor = Extension.getExtensionsAPIVersionMinor();
+    return (versionMajor == 3 && versionMinor == 2);
   }
 
   @Override
   public String getVersion() {
     return this.getClass().getPackage().getImplementationVersion();
+  }
+
+  @Override
+  public URL getURL() {
+    try {
+      return new URL("https://github.com/NTNU-IE-IIR/BlueJ-Checkstyle-Plugin/");
+    } catch (Exception e) {
+      return null;
+    }
   }
 
   @Override
